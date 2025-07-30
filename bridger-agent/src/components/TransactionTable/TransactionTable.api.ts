149b4c1c8243c097f737ced035a2293500feb842
Josh Roy <josh@pillar-advisors.com>
2025-07-30 14:14:20 -0400

    Add database getters for vendor and category dropdowns

--- bridger-agent/src/components/TransactionTable/TransactionTable.api.ts
+++ bridger-agent/src/components/TransactionTable/TransactionTable.api.ts
@@ -22,3 +22,41 @@ export const SEND_INFO_REQUEST = gql(`
             status
         }
     }`);
+
+export const GET_UNIQUE_VENDORS = gql`
+  query GetUniqueVendors {
+    getUniqueVendors
+  }
+`;
+
+export const GET_UNIQUE_CATEGORIES = gql`
+  query GetUniqueCategories {
+    getUniqueCategories
+  }
+`;
+
+export const UPDATE_TRANSACTION_VENDOR = gql`
+  mutation UpdateTransactionVendor($id: String!, $vendor: String!) {
+    updateTransactionVendor(id: $id, vendor: $vendor) {
+      success
+      message
+      transaction {
+        id
+        vendor
+      }
+    }
+  }
+`;
+
+export const UPDATE_TRANSACTION_CATEGORY = gql`
+  mutation UpdateTransactionCategory($id: String!, $category: String!) {
+    updateTransactionCategory(id: $id, category: $category) {
+      success
+      message
+      transaction {
+        id
+        category
+      }
+    }
+  }
+`;
